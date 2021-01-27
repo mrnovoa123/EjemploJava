288fbeddd4d8deea23b877a42bedfa903e8be333
mrnovoa123 <mr.novoa@gmail.com>
2021-01-27 17:35:11 +0100

    Update EjeGitHub.java
    Cambio en GitHub

--- src/ejegithub/EjeGitHub.java
+++ src/ejegithub/EjeGitHub.java
@@ -16,7 +16,7 @@ public class EjeGitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Maria");
+        System.out.println("Hola Profe");
     }
     
 }
